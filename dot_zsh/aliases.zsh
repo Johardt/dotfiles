e5a944a7b12888004e564030bedcd2754deedbd0
Joel Eckhardt <j.eckhardt8@icloud.com>
2025-07-02 13:58:55 +0200

    Starship config

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -1,17 +1,2 @@
 alias cm=chezmoi
 alias vi=nvim
-
-# Optional: Replace brew install and brew uninstall with brew add and brew remove
-brew() {
-  local subcommand="$1"
-  shift
-  case "$subcommand" in
-    install)
-      _brew_add "$@"
-      ;;
-    uninstall)
-      _brew_remove "$@"
-      ;;
-    # etc...
-  esac
-}
