1c7ec6fea966217be7f0033c723fe7b020a57971
Joel Eckhardt <j.eckhardt8@icloud.com>
2025-07-04 12:58:06 +0200

    KISS

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -15,27 +15,11 @@ alias less='bat'
 
 # zoxide aliases (cd replacement)
 alias cd='z'
-alias cdi='zi'  # Interactive cd with fzf
+alias cdi='zi'   # Interactive cd with fzf
 alias cdb='z -'  # Go back to previous directory
-
-# Improved grep
-alias grep='rg'
-alias fgrep='rg -F'
-alias egrep='rg -E'
 
 # Better find
 alias find='fd'
-
-# Git aliases (common ones)
-alias g='git'
-alias ga='git add'
-alias gc='git commit'
-alias gco='git checkout'
-alias gst='git status'
-alias gp='git push'
-alias gl='git pull'
-alias gd='git diff'
-alias gb='git branch'
 
 # Directory navigation
 alias ..='cd ..'
@@ -43,32 +27,8 @@ alias ...='cd ../..'
 alias ....='cd ../../..'
 alias ~='cd ~'
 
-# File operations with confirmation
-alias cp='cp -i'
-alias mv='mv -i'
-alias rm='rm -i'
-
 # Show hidden files by default
 alias lh='eza -la --color=always --group-directories-first --icons -a'
-
-# Quick directory listing
-alias lsd='eza -lD --color=always --group-directories-first --icons'  # List only directories
-alias lsf='eza -lF --color=always --group-directories-first --icons | rg -v "^d"'  # List only files
-
-# System monitoring
-alias top='top -o cpu'
-alias df='df -h'
-alias du='du -h'
-alias free='vm_stat'
-
-# Network
-alias ping='ping -c 5'
-alias myip='curl -s ifconfig.me'
-alias localip='ipconfig getifaddr en0'
-
-# Development
-alias python='python3'
-alias pip='pip3'
 
 # Reload shell
 alias reload='source ~/.zshrc'
