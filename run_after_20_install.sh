e3a6c80ef8969849856075a3479b83c13b4b5259
Joel Eckhardt <j.eckhardt8@icloud.com>
2025-07-14 19:47:29 +0200

    Improve after-install script

--- run_after_20_install.sh
+++ run_after_20_install.sh
@@ -2,3 +2,5 @@
 
 echo "Installing Homebrew packages from .Brewfile..."
 brew bundle install --file=~/.Brewfile
+
+echo "Installation complete! Please restart your terminal or run 'reload' to reload your shell configuration."
