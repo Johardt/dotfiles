660a23e8217e9845580deea39a62ce7cf9e375c0
Joel Eckhardt <johardt@proton.me>
2025-08-14 14:21:59 +0200

    Add pyenv, improve installation

--- run_after_20_install.sh
+++ run_after_20_install.sh
@@ -3,4 +3,7 @@
 echo "Installing Homebrew packages from .Brewfile..."
 brew bundle install --file=~/.Brewfile
 
+echo "Cleaning up..."
+brew cleanup
+
 echo "Installation complete! Please restart your terminal or run 'reload' to reload your shell configuration."
