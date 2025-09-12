8fb6ee716470642b9b2a9eb514ca9aae0d994e54
Joel Eckhardt <johardt@proton.me>
2025-09-12 21:50:46 +0200

    Optimize installation

--- run_after_20_install.sh
+++ run_after_20_install.sh
@@ -1,25 +1,4 @@
-#!/bin/bash
-
-# Ensure Homebrew is available in the current session
-if ! command -v brew &> /dev/null; then
-  echo "Setting up Homebrew environment..."
-  
-  # Detect Homebrew prefix based on architecture
-  if [[ "$(uname -m)" == "arm64" ]]; then
-    export BREW_PREFIX="/opt/homebrew"
-  else
-    export BREW_PREFIX="/usr/local"
-  fi
-  
-  # Source Homebrew environment if it exists
-  if [[ -x "$BREW_PREFIX/bin/brew" ]]; then
-    eval "$($BREW_PREFIX/bin/brew shellenv)"
-    echo "Homebrew environment loaded."
-  else
-    echo "Error: Homebrew installation not found at $BREW_PREFIX"
-    exit 1
-  fi
-fi
+#!/usr/bin/env bash
 
 echo "Installing Homebrew packages from .Brewfile..."
 brew bundle install --file=~/.Brewfile
--- run_once_after_30_install.sh
+++ run_once_after_30_install.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Install fisher and plugins inside fish
 # TODO: This needs to be more structured. And also, shell won't find fish at this point. Needs a new session or re-sourcing
-fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source; fisher install jorgebucaran/fisher jorgebucaran/autopair.fish; fisher install vitallium/tokyonight-fish'
+fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source; fisher install jorgebucaran/fisher jorgebucaran/autopair.fish;'
