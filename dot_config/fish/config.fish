d3026508595d9281e31dfcdf197bcd37e090b933
Joel Eckhardt <johardt@proton.me>
2025-09-26 15:23:13 +0200

    Fix path being corrupted in fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,15 +5,9 @@ source ~/.config/fish/local.fish
 
 fish_vi_key_bindings
 
-# Homebrew configuration for fish
-eval ($BREW_PREFIX/bin/brew shellenv)
-
 # Zoxide, fzf
 zoxide init fish | source
 fzf --fish | source
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH $HOME/.lmstudio/bin
 
 # Starship prompt
 eval (starship init fish)
