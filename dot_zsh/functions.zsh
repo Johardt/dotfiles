c3c0674d3dc5bf363e7b487112e45e6b8464b871
Joel Eckhardt <johardt@proton.me>
2025-12-16 14:19:52 +0100

    feat: Shell polishing, feature parity for zsh

--- dot_zsh/functions.zsh
+++ dot_zsh/functions.zsh
@@ -1,7 +1,16 @@
-function y() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-	yazi "$@" --cwd-file="$tmp"
-	IFS= read -r -d '' cwd < "$tmp"
-	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
-	rm -f -- "$tmp"
+function cx() {
+  if [[ $# -eq 0 ]]; then
+    cd ~
+  elif [[ $# -eq 1 ]]; then
+    # Using 'z' as in your Fish script
+    z "$1"
+  else
+    echo "Usage: cx [dir]"
+    return 1
+  fi
+
+  # Only list if the directory change was successful
+  if [[ $? -eq 0 ]]; then
+    eza --color=always --group-directories-first --icons
+  fi
 }
